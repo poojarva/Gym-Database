ac1528788866065934a0025236e130d35a8cc528
Pooja Patel <68351986+poojarva@users.noreply.github.com>
2022-04-21 21:00:03 -0400

    Add files via upload

--- mysql-php/www/connection.php
+++ mysql-php/www/connection.php
@@ -4,9 +4,9 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 $servername = "localhost"; // localhost means "itself" because the MySQL servers runs on the same server than the Apache webserver.
-$username = "acano";
-$password = "acanopasswd";
-$database = "humanresources_acano";
+$username = "patelp16";
+$password = "V00947239";
+$database = "project_patelp16";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
