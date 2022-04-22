38312fca277a10b5536f90ade9a506800bd19db4
Pooja Patel <68351986+poojarva@users.noreply.github.com>
2022-04-21 21:36:41 -0400

    Add files via upload

---
+++ mysql-php/www/login.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<title>HR database</title>
+<?php require_once('header.php'); ?>
+</head>
+
+<?php require_once('connection.php'); ?>
+
+<body>
+
+	<div class="container mt-3 mb-3">
+		<form method="post">
+			<div class="row justify-content-center">
+				<div class="col-4">
+					<div class="form-group">
+						<label>Email:</label>
+						<input type="email" class="form-control" id="email" placeholder="Enter email" name="email" required>
+					</div>
+					<div class="form-group">
+						<label>Password:</label>
+						<input type="password" class="form-control" id="password" placeholder="Enter password" name="password" required>
+					</div>
+					<button type="submit" class="btn btn-primary">Submit</button>
+				</div>
+			</div>
+		</form>
+	</div>
+
+</body>
+</html>
