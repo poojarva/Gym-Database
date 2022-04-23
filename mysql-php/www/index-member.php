48e2e4cfc33156c47b325dbc07faebcd8f036745
Pooja Patel <68351986+poojarva@users.noreply.github.com>
2022-04-23 10:13:28 -0400

    Add files via upload

---
+++ mysql-php/www/index-member.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>HR database</title>
+<?php require_once('header.php'); ?>
+</head>
+
+<?php require_once('connection.php'); ?>
+
+<body>
+
+<div class="container-fluid mt-3 mb-3">
+    <ul>
+    	<li><a href="classes.php">Basic information about classes</a></li>    
+    </ul>
+</div>
+
+</body>
+</html>
