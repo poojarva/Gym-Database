740df3822a884e4f7d31cc2176cdaffccc4912c4
Pooja Patel <68351986+poojarva@users.noreply.github.com>
2022-05-01 11:48:37 -0400

    Add files via upload

--- mysql-php/www/index.php
+++ mysql-php/www/index.php
@@ -70,8 +70,9 @@
                 <div class="main-button scroll-to-section">
    <!--  TODO: need to change it so if the button become member is clicked then it will prompt them to sign in or sign up -->
                     <a href="#features">Become a member</a>
-                    <a>Login In <?php require_once('connection.php'); ?>
+                    <a>Login In 
                     </a>
+                    <?php require_once('connection.php'); ?>
                 </div>
             </div>
         </div>
