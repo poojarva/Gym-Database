e285a0fb47493b5fecae2bfff932c48807018ce7
Pooja Patel <68351986+poojarva@users.noreply.github.com>
2022-05-06 19:16:39 -0400

    Add files via upload

--- mysql-php/www/index.php
+++ mysql-php/www/index.php
@@ -71,7 +71,7 @@
    <!--  TODO: need to change it so if the button become member is clicked then it will prompt them to sign in or sign up -->
                     <a href="#features">Become a member</a>
                     <a>Login In 
-                    <!-- ?php require_once('connection.php'); ?> -->
+                    <?php require_once('login.php'); ?>
                     </a>
                  
                 </div>
