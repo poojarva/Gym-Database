8c308c91d9fdc46b674ec95c649d437e7c6ad84f
Pooja Patel <68351986+poojarva@users.noreply.github.com>
2022-05-06 20:24:25 -0400

    Add files via upload

--- mysql-php/www/rooms-action.php
+++ mysql-php/www/rooms-action.php
@@ -7,26 +7,26 @@ class Rooms
     {
         global $conn;
         
-        $sqlQuery = "SELECT r.room_id as 'Room_Id', r.room_type 'Room_Type',  r.room_capacity as 'Room_Limit', l.location_name  as 'Room Location' FROM rooms r JOIN location l USING (location_id);";
+        $sqlQuery = "SELECT r.room_id as 'Room_Id', r.room_type 'Room_Type',  r.room_capacity as 'Room_Limit', l.location_name  as 'Room_Location' FROM rooms r JOIN location l USING (location_id);";
         
-        //         if (! empty($_POST["search"]["value"])) {
-        //             $sqlQuery .= 'WHERE (class_id LIKE "%' . $_POST["search"]["value"] . '%" OR class_name LIKE "%' . $_POST["search"]["value"] . '%") ';
-        //         }
+                if (! empty($_POST["search"]["value"])) {
+                    $sqlQuery .= 'WHERE (r.room_id LIKE "%' . $_POST["search"]["value"] . '%" OR r.room_type LIKE "%' . $_POST["search"]["value"] . '%") ';
+                }
         
-        //         if (! empty($_POST["order"])) {
-        //             $sqlQuery .= 'ORDER BY ' . ($_POST['order']['0']['column'] + 1) . ' ' . $_POST['order']['0']['dir'] . ' ';
-        //         } else {
-        //             $sqlQuery .= 'ORDER BY class_name DESC ';
-        //         }
+                if (! empty($_POST["order"])) {
+                    $sqlQuery .= 'ORDER BY ' . ($_POST['order']['0']['column'] + 1) . ' ' . $_POST['order']['0']['dir'] . ' ';
+                } else {
+                    $sqlQuery .= 'ORDER BY r.room_type DESC ';
+                }
         
         $stmt = $conn->prepare($sqlQuery);
         $stmt->execute();
         
         $numberRows = $stmt->rowCount();
         
-        //         if ($_POST["length"] != - 1) {
-        //             $sqlQuery .= 'LIMIT ' . $_POST['start'] . ', ' . $_POST['length'];
-        //         }
+                if ($_POST["length"] != - 1) {
+                    $sqlQuery .= 'LIMIT ' . $_POST['start'] . ', ' . $_POST['length'];
+                }
         
         
         $dataTable = array();
@@ -37,11 +37,11 @@ class Rooms
             $dataRow[] = $sqlRow['Room_Id'];
             $dataRow[] = $sqlRow['Room_Type'];
             $dataRow[] = $sqlRow['Room_Limit'];
-            $dataRow[] = $sqlRow['Room Location'];
+            $dataRow[] = $sqlRow['Room_Location'];
 
             
-            $dataRow[] = '<button type="button" name="update" emp_id="' . $sqlRow["Class_Id"] . '" class="btn btn-warning btn-sm update">Book Room</button>
-                          <button type="button" name="delete" emp_id="' . $sqlRow["Class_Id"] . '" class="btn btn-danger btn-sm delete">Unbook Room</button>';
+            $dataRow[] = '<button type="button" name="update" emp_id="' . $sqlRow["Room_Id"] . '" class="btn btn-warning btn-sm update">Book Room</button>
+                          <button type="button" name="delete" emp_id="' . $sqlRow["Room_Id"] . '" class="btn btn-danger btn-sm delete">Unbook Room</button>';
             
             $dataTable[] = $dataRow;
         }
