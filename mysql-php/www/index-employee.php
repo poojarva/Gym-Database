a668a67443a66f6cc10397bdaaabe466fcc4263a
Pooja Patel <68351986+poojarva@users.noreply.github.com>
2022-05-08 00:07:24 -0400

    adding style to index

--- mysql-php/www/index-employee.php
+++ mysql-php/www/index-employee.php
@@ -16,6 +16,10 @@
  }
 
 </style>
+
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
 <?php require_once('connection-employee.php'); ?>
