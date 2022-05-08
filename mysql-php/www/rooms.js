9f7a62948b40f2a34f23d4add536bfc1ac5a16a6
Pooja Patel <68351986+poojarva@users.noreply.github.com>
2022-05-08 17:02:14 -0400

    fixing room table

--- mysql-php/www/rooms.js
+++ mysql-php/www/rooms.js
@@ -1,19 +1,24 @@
 $(document).ready(function(){
 	
-	$('#table-rooms').DataTable({
+		var tableRooms = $('#table-rooms').DataTable({
 		"dom": 'Blfrtip',
-		"ordering": false,
-		"searching": false,
-		"paging": false,
+		"autoWidth": false,
+		"processing":true,
+		"serverSide":true,
+		"pageLength":15,
+		"lengthMenu":[[15, 25, 50, 100, -1], [15, 25, 50, 100, "All"]],
 		"responsive": true,
+		"language": {processing: '<i class="fa fa-spinner fa-spin fa-2x fa-fw"></i>'},
+		"order":[],
 		"ajax":{
 			url:"rooms-action.php",
 			type:"POST",
 			data:{
 					action:'listRooms'
-				 },
+				},
 			dataType:"json"
 		}
-	});
+	});	
+	
 	
 });
