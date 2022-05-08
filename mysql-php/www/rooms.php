671bbad6f45ff818d7500f328767b4488fed30fd
Pooja Patel <68351986+poojarva@users.noreply.github.com>
2022-05-08 17:06:53 -0400

    Add files via upload

--- mysql-php/www/rooms.php
+++ mysql-php/www/rooms.php
@@ -3,10 +3,19 @@
 <title>Rooms</title>
 <?php require_once('header.php'); ?>
 <!-- My JS libraries -->
+<script src="https://kit.fontawesome.com/aec5ef1467.js"></script>
+
+<!-- JS libraries for datatables buttons-->
+<script src="https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.7.1/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.5/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.5/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.print.min.js"></script>
 <script src="rooms.js"></script>
 </head>
 
-<?php require_once('connection.php'); ?>
+<?php require_once('connection-member.php'); ?>
 
 <body>
 
