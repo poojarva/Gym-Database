36a9d2f4b07f1e39bbadaba3de0db997e2200d79
Pooja Patel <68351986+poojarva@users.noreply.github.com>
2022-05-08 20:11:37 -0400

    Add files via upload

--- mysql-php/www/rooms-action.php
+++ mysql-php/www/rooms-action.php
@@ -9,24 +9,24 @@ class Rooms
         
         $sqlQuery = "SELECT r.room_id as 'ID', r.room_type 'room_type',  r.limit_capacity as 'limit_capacity', r.max_limit as 'max_limit',  l.location_id  as 'location_id' FROM rooms r JOIN location l USING (location_id)";
         
-        if (!empty($_POST["search"]["value"])) {
-            $sqlQuery .= 'WHERE (r.room_type LIKE "%' . $_POST["search"]["value"] . '%") ';
-        }
+//         if (!empty($_POST["search"]["value"])) {
+//             $sqlQuery .= 'WHERE (r.room_type LIKE "%' . $_POST["search"]["value"] . '%") ';
+//         }
         
-        if (!empty($_POST["order"])) {
-            $sqlQuery .= 'ORDER BY ' . ($_POST['order']['0']['column'] + 1) . ' ' . $_POST['order']['0']['dir'] . ' ';
-        } else {
-            $sqlQuery .= 'ORDER BY r.room_type DESC ';
-        }
+//         if (!empty($_POST["order"])) {
+//             $sqlQuery .= 'ORDER BY ' . ($_POST['order']['0']['column'] + 1) . ' ' . $_POST['order']['0']['dir'] . ' ';
+//         } else {
+//             $sqlQuery .= 'ORDER BY r.room_type DESC ';
+//         }
         
         $stmt = $conn->prepare($sqlQuery);
         $stmt->execute();
         
         $numberRows = $stmt->rowCount();
         
-        if ($_POST["length"] != - 1) {
-            $sqlQuery .= 'LIMIT ' . $_POST['start'] . ', ' . $_POST['length'];
-        }
+//         if ($_POST["length"] != - 1) {
+//             $sqlQuery .= 'LIMIT ' . $_POST['start'] . ', ' . $_POST['length'];
+//         }
         
         
         $dataTable = array();
@@ -59,18 +59,22 @@ class Rooms
     {
         global $conn;
         if ($_POST["ID"]) {
+            // need to first add the user and this room
             
-            $sqlQuery = "DELETE FROM users_rooms WHERE room_id = :room_id;";
+          
             
+            $sqlQuery = "INSERT INTO users_rooms (username_id, room_id) VALUES (:username_id, :room_id);";
             $stmt = $conn->prepare($sqlQuery);
             $stmt->bindValue(':room_id', $_POST["ID"]);
-            $stmt->execute();
+            $stmt->bindValue(':username_id', $_SESSION['username_id']);
             
-            $sqlQuery = "DELETE FROM rooms WHERE room_id = :room_id;";
-            
-            $stmt = $conn->prepare($sqlQuery);
-            $stmt->bindValue(':room_id', $_POST["ID"]);
             $stmt->execute();
+         
+//             $sqlQuery = "DELETE FROM rooms WHERE room_id = :room_id;";
+            
+//             $stmt = $conn->prepare($sqlQuery);
+//             $stmt->bindValue(':room_id', $_POST["ID"]);
+//             $stmt->execute();
         }
     }
     
