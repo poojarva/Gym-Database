2fb4c3e64a6c497965bd9df6f693752b18209528
Pooja Patel <68351986+poojarva@users.noreply.github.com>
2022-05-09 23:02:56 -0400

    Add files via upload

---
+++ mysql-php/www/connection-new.php
@@ -0,0 +1,44 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$servername = "localhost"; // localhost means "itself" because the MySQL servers runs on the same server than the Apache webserver.
+$username = "patelp16";
+$password = "V00947239";
+$database = "project_patelp16";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+
+// Start or resume session variables
+session_start();
+
+    // If the page is receiving the email and password from the login form then verify the login data
+if (isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['location_id']))
+    {
+        $stmt = $conn->prepare("INSERT INTO users (first_name, last_name, email, password, type, location_id) VALUES (:first_name, :last_name, :email, :password, :location_id)");
+        $stmt->bindValue(':first_name', $_POST['first_name']);
+        $stmt->bindValue(':last_name', $_POST['last_name']);
+        $stmt->bindValue(':email', $_POST['email']);
+        $stmt->bindValue(':password', password_hash($_POST["password"], PASSWORD_DEFAULT));
+        $stmt->bindValue(':location_id', $_POST['location_id']);
+        
+        $stmt->execute();
+        
+        echo "You have successfully become a member of our gym! Continue back to the home page and sign in!";
+        header("location: index.php");
+    }
+    else
+    {
+        // Show login page
+        require('sign-up.php');
+        exit();
+   }
+
+?>
--- mysql-php/www/sign-up.php
+++ mysql-php/www/sign-up.php
@@ -1,127 +1,10 @@
-<?php
-// Include config file
-
-// Define variables and initialize with empty values
-$email = $password = $first_name = $last_name = "";
-$location_id = 0;
-$email_err = $password_err = $firstname_err = $lastname_err = $location_id_err = "";
-
-// Processing form data when form is submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    // Validate first_name
-    if (empty(trim($_POST["first_name"]))) {
-        $firstname_err = "Please enter your first name.";
-    } else {
-        $first_name = trim($_POST["first_name"]);
-    }
-
-    // Validate last_name
-    if (empty(trim($_POST["last_name"]))) {
-        $lastname_err = "Please enter your last name.";
-    } else {
-        $last_name = trim($_POST["last_name"]);
-    }
-
-    // Validate email
-    if (empty(trim($_POST["email"]))) {
-        $email_err = "Please enter a email.";
-    } else {
-        $email = trim($_POST["email"]);
-        
-        // Prepare a select statement
-        $sql = "SELECT username_id FROM users WHERE email = ?";
-
-        if ($stmt = mysqli_prepare($link, $sql)) {
-
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $email);
-
-            // Attempt to execute the prepared statement
-            if (mysqli_stmt_execute($stmt)) {
-                /* store result */
-                mysqli_stmt_store_result($stmt);
-
-                if (mysqli_stmt_num_rows($stmt) > 0) {
-                    $email_err = "This email is already taken. Try another one!";
-                } else {
-                    $email = trim($_POST["email"]);
-                }
-            } else {
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-
-            // Close statement
-            mysqli_stmt_close($stmt);
-        }
-    }
-
-    // Validate password
-    if (empty(trim($_POST["password"]))) {
-        $password_err = "Please enter a password.";
-    }
-    else {
-        $password = trim($_POST["password"]);
-    }
-    
-    if (empty(trim($_POST["location_id"]))) {
-        $location_id_err = "Please choose a location.";
-    }
-    else {
-        $location_id = trim($_POST["location_id"]);
-    }
-      
-
-   
-    }
-
-    if (empty($email_err) && empty($password_err) && empty($firstname_err) && empty($lastname_err) && empty($location_id_err)) {
-
-        // Prepare an insert statement
-        $sql = "INSERT INTO users (first_name, last_name, email, password, type, location_id) VALUES (?, ?, ?, ?, ?, ?)";
-
-        try {
-        if ($stmt = mysqli_prepare($link, $sql)) {
-
-            // Set parameters
-            $p_email = $email;
-            $p_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
-            $p_firstname = $first_name;
-            $p_lastname = $last_name;
-            $p_locationid = $location_id;
-            $p_type = 'Member';
-            
-          
-
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt,'sssss', $p_firstname, $p_lastname, $p_email, $p_password, $p_type, $p_locationid);
-
-            // Attempt to execute the prepared statement
-            if (mysqli_stmt_execute($stmt)) {
-                        
-                 echo "You have successfully become a member of our gym! Continue back to the home page and sign in!";
-                header("location: index.php");
-                
-            } 
-        }
-    } catch (PDOException $e) {
-        echo "Error: " . $e->getMessage();
-    }
-
-    // Close statement
-    mysqli_stmt_close($stmt);
-}
-
-?>
-
-
-
 <html>
 <head>
 <title>Gym Database</title>
 <?php require_once('header.php'); ?>
 </head>
 
+<?php require_once('connection-new.php'); ?>
 
 <body>
 
@@ -130,12 +13,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			<div class="row justify-content-center">
 				<div class="col-4">
 				<div class="form-group">
-						<label>First Name</label>
-						<input type="first_name" class="form-control" id="text" placeholder="Enter First Name" name="first_name" required>
+						<label>First Name:</label>
+						<input type="text" class="form-control" id="first_name" placeholder="Enter First Name" name="first_name" required>
 					</div>
 					<div class="form-group">
-						<label>Last Name</label>
-						<input type="last_name" class="form-control" id="text" placeholder="Enter Last Name" name="last_name" required>
+						<label>Last Name:</label>
+						<input type="text" class="form-control" id="last_name" placeholder="Enter Last Name" name="last_name" required>
 					</div>
 					<div class="form-group">
 						<label>Email:</label>
@@ -145,11 +28,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 						<label>Password:</label>
 						<input type="password" class="form-control" id="password" placeholder="Enter password" name="password" required>
 					</div>
-				</div>
-				<div class="form-group">
-						<label>Location ID</label>
-<!-- 					<input type="password" class="form-control" id="password" placeholder="Enter password" name="password" required> -->
-						
+					<div class="form-group">
+						<label>Location ID:</label>
 						<select class="form-control" id="location_id" required>
             			    <?php
             			    global $conn;
@@ -162,21 +42,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             			        }
                             ?>
             			</select>
-            			</div>
-            			
-            			<button type="submit" class="btn btn-primary">Submit</button>
-            			
-            			
-            			<div class="form-group">
-							<input type="submit" class="btn btn-primary" value="Submit"> 
-                            <input type="reset" class="btn btn-secondary ml-2" value="Reset">
-						</div>
-						<p>
-							Already have an account? <a href="login.php">Login here</a>.
-						</p>
+					</div>
+					<button type="submit" class="btn btn-primary">Submit</button>
+				</div>
+				<p>
+				Already have an account? 
+				<a href="login-member.php">Login here</a>.
+				</p>
 			</div>
 		</form>
 	</div>
 
 </body>
 </html>
+	
